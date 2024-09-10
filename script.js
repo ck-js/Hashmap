67145d9e0b8ccab0426401c95b21821f1b5cbdee
ck-js <ckentertainmentinfo@gmail.com>
2024-09-10 20:54:48 +0700

    add the keys, values, clear and entries hashmap methods as well as the size and loadfactor constructor to initialize the the size which keeps track of key value pairs added and the load factor threshhold which is calculated by dividing the number of key value pairs or buckets by the bucket size. We add to bottom of set method with condition check and if threshold is greater than our set load factor we call the resize method which makes a copy of the current bucket as old buckets, set the new length of buckets, set size of pairs to 0 then use for loop to loop through each old buckets to be called in the set method inside of a while loop for when current bucket is true.

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ class ListNode {
 }
 class Hashmap {
     constructor() {
-        this.buckets = new Array(17);
+        this.buckets = new Array(8);
+        this.size = 0;
+        this.loadFactor = 0.7
     }
     hash(key) {
         let hashCode = 3;
@@ -23,11 +25,31 @@ class Hashmap {
      
         return hashCode;
       }
+    resize() {
+const oldBuckets = this.buckets;
+this.buckets = new Array(oldBuckets.length * 2)
+this.size = 0;
+for (let i = 0; i < oldBuckets.length; i++) {
+    let currentBucket = oldBuckets[i]
+    while (currentBucket) {
+        this.set(currentBucket.key, currentBucket.value);
+        currentBucket = currentBucket.next;
+
+
+    }
+}
+
+
+
+
+    }
+
       set(key, value) {
         const index = this.hash(key) % this.buckets.length;
 
         if (!this.buckets[index]) {
             this.buckets[index] = new ListNode(key, value);
+            this.size++;
         } else {
             let currentBucket = this.buckets[index];
     
@@ -35,16 +57,22 @@ class Hashmap {
                 if (currentBucket.key === key) {
                     currentBucket.value = value; // Update value if key already exists
                     
-                                        return;
+            return;
                 }
                 if (!currentBucket.next) {
                     currentBucket.next = new ListNode(key, value); // Add new node at the end
+                    this.size++;
 
                     return;
                 }
                 currentBucket = currentBucket.next;
             }
         }
+    if (this.size / this.buckets.length > this.loadFactor) {
+        this.resize();
+    }
+    
+    
     }
       get(key) {
 const index = this.hash(key) % this.buckets.length;
@@ -97,7 +125,45 @@ return null
         }
         return count;
     }
+clear() {
+    this.buckets = new Array(17);
+    
+}
+keys() {
+    const keys = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+        let currentBucket = this.buckets[i];
+        while (currentBucket) {
+            keys.push(currentBucket.key)
+            currentBucket = currentBucket.next
 
+        }
+    }
+    return keys
+}
+values() {
+const values = [];
+for (let i = 0; i < this.buckets.length; i++) {
+let currentBucket = this.buckets[i];
+while (currentBucket){
+    values.push(currentBucket.value)
+    currentBucket = currentBucket.next;
+}
+}    
+return values;
+}
+entries() {
+    const entries = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+        let currentBucket = this.buckets[i];
+        while (currentBucket) {
+            entries.push([currentBucket.key, currentBucket.value])
+            currentBucket = currentBucket.next;
+        }
+    }
+return entries;
+
+}
 
      
 }
@@ -110,11 +176,21 @@ const hashmap = new Hashmap();
 hashmap.set("Carlos", "Developer");
 // hashmap.set("Carlos", "Designer");
 hashmap.set("Carla", "Frontend Developer");
+hashmap.set("arla", "Frontend Developer");
+hashmap.set("rla", "Frontend Developer");
+hashmap.set("Car", "Frontend Developer");
+hashmap.set("Ca", "Frontend Developer");
+// hashmap.set("Casssr", "Frontend Developer");
 // hashmap.set("Carla", "Backend Developer");
 // console.log(hashmap.get('Carla'));
 // console.log(hashmap.has('arlos'));
 // console.log(hashmap.remove("Carla"))
-console.log(hashmap.length());
+// console.log(hashmap.length());
+// console.log(hashmap.clear());
+// console.log(hashmap.keys());
+// console.log(hashmap.values());
+// console.log(hashmap.entries());
+console.log(hashmap.buckets.length);
 console.log(hashmap);
 
 // if (index < 0 || index >= buckets.length) {
